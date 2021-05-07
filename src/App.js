af55899b3ab2480255c457528631a624d93a9d80
Kierapapa <kierapapa@adwebzine.com>
2021-05-07 14:45:49 -0700

    Practice JSXandProps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const City = ({location}) => {
+  return <p>I have lived in {location}</p>
 }
 
+
+const citiesIveLived = [{id:1, name:"Seoul"},{id:2,name:"Tokyo"},{id:3, name:"Vancouver"}];
+
+const App = () => {
+  return (
+    
+    <div><h1>Hello React</h1>
+    {citiesIveLived.map(renderCity())}
+    </div>
+  )
+}
 export default App;
+
+function renderCity() {
+  return city => <City key={city.id} location={city.name} />;
+}
